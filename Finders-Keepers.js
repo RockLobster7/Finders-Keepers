a2d77204c896a4d37e9308666498159e1b09f8c3
Mario Cueva <mario.cueva.au@gmail.com>
2017-09-30 12:08:38 +1000

    completed challenge

--- Finders-Keepers.js
+++ Finders-Keepers.js
@@ -4,10 +4,16 @@ Create a function that looks through an array (first argument) and returns the f
 */
 
 function findElement(arr, func) {
-    var num = 0;
-    return num;
-  }
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i])) return(arr[i]);
+    }
+return ;
+}
 
-  
-  console.log(findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; })); // should return 8.
-  console.log(findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; })); // should return undefined.
+
+console.log(findElement([1, 3, 5, 8, 9, 10], function (num) {
+    return num % 2 === 0;
+})); // should return 8.
+console.log(findElement([1, 3, 5, 9], function (num) {
+    return num % 2 === 0;
+})); // should return undefined.
